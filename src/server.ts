6c27827ae9f5c98e063ac47452aa11ac060e679c
walk8243 <walk8243@gmail.com>
2019-09-03 02:53:30 +0900

    サーバーの基本プログラムを作成

---
+++ src/server.ts
@@ -0,0 +1,20 @@
+import net from 'net';
+
+const server = net.createServer((connection) => {
+  connection.on('connect', () => {
+    console.log('connected.');
+  });
+  connection.on('close', () => {
+    console.log('disconnected.');
+  });
+  connection.on('data', (data) => {
+    console.log(data.toString());
+  });
+  connection.on('error', (err) => {
+    console.error(err.message);
+  });
+  connection.write('unix domain socket');
+  connection.end();
+});
+
+server.listen('/tmp/unix.sock');
